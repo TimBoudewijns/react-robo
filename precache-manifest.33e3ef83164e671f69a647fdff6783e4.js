d4efbff018086b83d700bab4ffc0ba61cb5e10b4
TimBoudewijns <51700403+TimBoudewijns@users.noreply.github.com>
2020-01-08 12:12:03 +0100

    Updates

---
+++ precache-manifest.33e3ef83164e671f69a647fdff6783e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e68557b78c65129c3fa274a10e0e930",
+    "url": "/react-robo/index.html"
+  },
+  {
+    "revision": "38cfee84dfb97f9c04b5",
+    "url": "/react-robo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d931f1f440a5a27d8aed",
+    "url": "/react-robo/static/css/main.8cc60ff5.chunk.css"
+  },
+  {
+    "revision": "38cfee84dfb97f9c04b5",
+    "url": "/react-robo/static/js/2.8f026137.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-robo/static/js/2.8f026137.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d931f1f440a5a27d8aed",
+    "url": "/react-robo/static/js/main.28260c78.chunk.js"
+  },
+  {
+    "revision": "b789501ee2d3bd051fc5",
+    "url": "/react-robo/static/js/runtime-main.5554d819.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
